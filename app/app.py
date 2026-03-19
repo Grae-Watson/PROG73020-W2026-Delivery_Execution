28868f8c45cf21ad6d0a39a9a3424036b9c8394c
Jordan Paterson <jpaterson7992@conestogac.on.ca>
2026-03-19 15:28:05 -0400

    Added UI from customer repo to integration repo

---
+++ app/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template, request, jsonify, make_response
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+@app.route('/login')
+def login():
+   return render_template('login.html')
+
+@app.route('/providers')
+def providers():
+   return render_template('providers.html')
+
+@app.route('/info')
+def info():
+   return render_template('info.html')
+ 
+
+   
+   
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=7500, debug=True)
