5acb4a6e7dc46466c46d9177df8162ca4615bc62
TylerDdao <baonamdao05@gmail.com>
2026-03-28 00:09:18 -0400

    Update app.py
    Added an API to take restock request from product team

--- app/app.py
+++ app/app.py
@@ -17,9 +17,32 @@ def providers():
 @app.route('/info')
 def info():
    return render_template('info.html')
- 
 
-   
+# Restock request (Prod team - Supply team)
+@app.route('/api/v1/restock_request', methods = ["GET", "POST"])
+def restock_request():
+   if request.method == "POST":
+      if request.headers.get("X-API-Key") != "bestTeam":
+         return {
+            "status": "error",
+            "data": None,
+            "error": {
+                "code": "UNAUTHORIZED",
+                "message": "Invalid team's secret or your team don't have permission for this API"
+            }
+         }, 401
+      data = request.get_json()
+      # Supposed to save restock to a database (Will be working with Data team)
+      print(f"vendorId: {data.get("vendorId")}")
+      print(f"manifest: {data.get("manifest")}")
+      return {
+            "status": "sucess",
+            "data": None,
+            "error": None
+         }, 200
+      
+
+
    
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=7500, debug=True)
